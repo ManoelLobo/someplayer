d879bf193e3dd64f987acdfba8fe73b2cf38345b
Manoel Lobo <manoel.lobo@gmail.com>
2017-11-29 16:39:32 -0300

    Trending Songs
    Reducers com reduxsauce

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,7 @@
 import { combineReducers } from 'redux';
 
 import navReducer from 'navigation/reducer';
+import { reducer as trendingSongs } from './ducks/trending-songs';
 
 import configureStore from './configureStore';
 import rootSaga from './sagas';
@@ -8,6 +9,7 @@ import rootSaga from './sagas';
 export default () => {
   const rootReducer = combineReducers({
     nav: navReducer,
+    trendingSongs,
   });
 
   return configureStore(rootReducer, rootSaga);
